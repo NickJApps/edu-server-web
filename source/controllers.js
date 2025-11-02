9d1cd93ca91e808a3460b412d98f31d4249238d5
NickJApps <nicolasfr74@gmail.com>
2025-11-02 11:06:10 +0300

    controller_data_let

--- source/controllers.js
+++ source/controllers.js
@@ -6,16 +6,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export function mainPage(res) {
-    const filePath = path.join(__dirname, 'main.html');
-
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Server error');
-            return;
-        }
-
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(data);
-    });
+    let data = '<!DOCTYPE html>' + 
+        '< html lang = "en" >' +
+            '<head>'+
+                '<meta charset="UTF-8">' +
+                    '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+                        '<title>Target</title>' + 
+                    '</head>' +
+                    '<body>' +
+                        '<h1>Planned activities</h1>' +
+                    '</body>' +
+                '</html>';
+     res.end(data);
 }
